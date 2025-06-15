4daa50bace2c53d8c7e109b05dcb9bd9b947b1b5
heyamramadan <arwahossam586@gmail.com>
2025-06-15 21:51:15 +0200

    تعديل

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,14 +7,17 @@ use Illuminate\Foundation\Configuration\Middleware;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware) {
+    ->withMiddleware(callback: function (Middleware $middleware) {
          $middleware->alias([
             'admin' => \App\Http\Middleware\AdminMiddleware::class,
          'adminOrEmployee' => \App\Http\Middleware\AdminOrEmployeeMiddleware::class,
 
+            // ✅ Middleware الخاص بـ Sanctum
+            'auth:sanctum' => \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
