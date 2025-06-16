421c8fc13c0ca089d40e5aa5fe7f912148ebbd5d
Mohimem <sulimangzllal@alMacBook-Pro-mohimen-tantoush.local>
2025-06-16 19:48:01 +0200

    fix api issue

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -8,6 +8,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         api: __DIR__.'/../routes/api.php',
+  
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
@@ -17,7 +18,7 @@ return Application::configure(basePath: dirname(__DIR__))
          'adminOrEmployee' => \App\Http\Middleware\AdminOrEmployeeMiddleware::class,
 
             // ✅ Middleware الخاص بـ Sanctum
-            'auth:sanctum' => \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+    
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,13 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\ParentAuthController;
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+
+
 
 Route::post('/login-parent', [ParentAuthController::class, 'login']);
