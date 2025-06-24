f85edc035ac3034a5760dea49d43f9598cc4787e
heyamramadan <arwahossam586@gmail.com>
2025-06-24 17:23:31 +0200

    تعديل

--- routes/api.php
+++ routes/api.php
@@ -7,18 +7,27 @@ use App\Http\Controllers\Api\ParentStudentController;
 use App\Http\Controllers\Api\ApiWalletController;
 use App\Http\Controllers\Api\ApiBannedProductController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware(middleware: 'auth:sanctum');
-//تسجيل دخول
+// تسجيل دخول
 Route::post('/login-parent', [ParentAuthController::class, 'login']);
-//طلاب
+
+// المسارات المحمية ب auth:sanctum
+Route::middleware('auth:sanctum')->group(function () {
+
+    // جلب بيانات المستخدم
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+    // طلاب ولي الأمر
     Route::get('/parent/students', [ParentStudentController::class, 'getMyStudents']);
-//
-  Route::get('/wallet', [ApiWalletController::class, 'getWallet']);
+
+    // المحفظة
+    Route::get('/wallet', [ApiWalletController::class, 'getWallet']);
     Route::post('/wallet/daily-limit', [ApiWalletController::class, 'updateDailyLimit']);
 
     // المنتجات الممنوعة
     Route::get('/banned-products', [ApiBannedProductController::class, 'index']);
     Route::post('/banned-products', [ApiBannedProductController::class, 'store']);
     Route::delete('/banned-products/{ban_id}', [ApiBannedProductController::class, 'destroy']);
+
+});
