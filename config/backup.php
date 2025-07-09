9e675c06f0491593c4d1396804fb4e526286d480
heyamramadan <arwahossam586@gmail.com>
2025-07-09 18:27:17 +0200

    تعديل

--- config/backup.php
+++ config/backup.php
@@ -13,6 +13,9 @@ return [
                     base_path('vendor'),
                     base_path('node_modules'),
                 ],
+                'follow_links' => false,
+                'ignore_unreadable_directories' => false,
+                'relative_path' => base_path(''),
             ],
             'databases' => [
                 'mysql',
@@ -28,10 +31,13 @@ return [
     ],
 
     'cleanup' => [
+        'strategy' => \Spatie\Backup\Tasks\Cleanup\Strategies\DefaultStrategy::class,
         'default_strategy' => [
             'keep_all_backups_for_days' => 7,
             'keep_daily_backups_for_days' => 16,
             'keep_weekly_backups_for_weeks' => 8,
+            'keep_monthly_backups_for_months' => 4,
+            'keep_yearly_backups_for_years' => 2,
             'delete_oldest_backups_when_using_more_megabytes_than' => 5000,
         ],
     ],
--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,13 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+// جدولة أمر النسخ الاحتياطي يوميًا الساعة 00:00
+Schedule::command('backup:run')->daily()->at('00:00');
+
+// جدولة أمر تنظيف النسخ القديمة يوميًا الساعة 01:00
+Schedule::command('backup:clean')->daily()->at('01:00');
