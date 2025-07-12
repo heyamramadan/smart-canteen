7ea8217a3487ff96eb3e47b156e7041efb9cac66
heyamramadan <arwahossam586@gmail.com>
2025-07-12 21:09:04 +0200

    تعديل

---
+++ database/migrations/2025_07_12_183713_alter_phone_number_in_users_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+            Schema::table('users', function (Blueprint $table) {
+            // تعديل الحقل ليصبح not null
+            $table->string('phone_number')->nullable(false)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            // التراجع بجعل الحقل nullable مرة أخرى
+            $table->string('phone_number')->nullable()->change();
+        });
+    }
+};
