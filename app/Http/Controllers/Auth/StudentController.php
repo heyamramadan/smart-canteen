54a13dad3743d383580cb8549eab6c65761c418f
heyamramadan <arwahossam586@gmail.com>
2025-07-21 15:52:46 +0200

    تعديل

--- app/Http/Controllers/Auth/StudentController.php
+++ app/Http/Controllers/Auth/StudentController.php
@@ -40,7 +40,7 @@ class StudentController extends Controller
             'class' => $validated['class'],
             'user_id' => $validated['user_id'],
             'father_name' => $fatherName,
-            'pin_code' => mt_rand(100000, 999999),
+            'pin_code' => mt_rand(1000, 9999),
         ];
 
         if ($request->hasFile(key: 'image')) {
