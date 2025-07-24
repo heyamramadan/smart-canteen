899e982d04f2bb1c5e7fc57e2436d8dd8ccf738b
heyamramadan <arwahossam586@gmail.com>
2025-07-25 00:18:20 +0200

    تعديل

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -3,6 +3,8 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
     <title>إدارة المستخدمين - لوحة تحكم المقصف</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <script>
