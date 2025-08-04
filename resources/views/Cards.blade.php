42d58a19ad7af77550bddd60849519278a514ed2
heyamramadan <arwahossam586@gmail.com>
2025-08-04 22:59:34 +0200

    وصف التغييرات

--- resources/views/Cards.blade.php
+++ resources/views/Cards.blade.php
@@ -116,7 +116,8 @@
     </div>
   </div>
 <!-- مودال رسالة الخطأ -->
-<div id="errorModal" class="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center hidden">
+<div id="errorModal" class="fixed inset-0 bg-black bg-opacity-60 flex items-center justify-center z-50">
+
   <div class="bg-white rounded-lg shadow-lg p-6 max-w-sm text-center">
    <p id="errorMessage" class="text-orange-600 text-lg mb-4"></p>
 
