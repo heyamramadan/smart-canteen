e1b3dbeb913d20248584151d1086ff65390d9c4c
heyamramadan <arwahossam586@gmail.com>
2025-08-07 23:03:56 +0200

    تعديل

--- app/Http/Controllers/Auth/UserController.php
+++ app/Http/Controllers/Auth/UserController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
-use App\Models\Wallet; // <-- إضافة موديل المحفظة
+use App\Models\Wallet; 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
